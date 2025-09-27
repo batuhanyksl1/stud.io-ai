45b21c3e353a3e78323d40179757a103372c79b5
batuhanyksl1 <batuhanyksl1@gmail.com>
2025-09-28 02:26:13 +0300

    Firebase Crashlytics entegrasyonu yapıldı. firebase.config.ts dosyasına crashlytics import edildi ve export edildi. Android ve iOS yapılandırma dosyalarında gerekli güncellemeler yapıldı. Ayrıca, package-lock.json dosyasında bağımlılık güncellemeleri gerçekleştirildi.

--- firebase.config.ts
+++ firebase.config.ts
@@ -3,7 +3,8 @@
 // Bu dosya sadece import'lar için kullanılır
 
 import auth from "@react-native-firebase/auth";
+import crashlytics from "@react-native-firebase/crashlytics"; // Bu satırı ekleyin
 import firestore from "@react-native-firebase/firestore";
 import storage from "@react-native-firebase/storage";
 
-export { auth, firestore, storage };
+export { auth, crashlytics, firestore, storage }; // crashlytics'i export edin
